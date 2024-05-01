3be53ebe19ffaf956ca635faa304209cacd4ecdd
VJgnus <vedantbhong3211@gmail.com>
2024-05-01 08:57:38 -0400

    Tests

--- src/test/java/CalculatorTests.java
+++ src/test/java/CalculatorTests.java
@@ -2,46 +2,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
 import model.CalcModel;
-import model.Operation;
+
 
 class CalculatorTests
 {
 
 	CalcModel model;
 	
-	DoubleProperty num1 = new SimpleDoubleProperty();
-	DoubleProperty num2 = new SimpleDoubleProperty();
-	DoubleProperty result = new SimpleDoubleProperty();
+	
 	@BeforeEach
 	void setUp() throws Exception
 	{
 		model = new CalcModel();
-		num1.set(10);
-		num2.set(5);;
+		
 	}
 
 	@Test
-	void test()
-	{
-		testSetNums();
-		testAdd();
+		void add() {
+	        setNums(8, 3);
+	        model.add();
+	        assertEquals(11, getResultVal());
+	    }
 
+	private Double getResultVal()
+	{
+		return model.getResult().getValue();
 	}
 
-	public void testSetNums()
-	{
-		model.setNum1(num1);
-		model.setNum2(num2);
-		assertEquals(model.getNum1().get(), 10);
-		assertEquals(model.getNum2().get(), 5);
-	}
-	public void testAdd()
-	{
-		model.add();
-		assertEquals(model.getResult().get(), 15);
-	}
+	private void setNums(int num1, int num2) {
+        model.getNum1().setValue(num1);
+        model.getNum2().setValue(num2);
+    }
+
 }
